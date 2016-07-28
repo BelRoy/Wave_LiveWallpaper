11c47c9ad24c2969afc818bd294359c492820bff
BelRoy <oksiwhite@ukr.net>
2016-07-28 19:01:57 +0300

    Initial commit

---
+++ app/src/main/java/com/devqt/wavelivewallpaper/AnimationService.java
@@ -0,0 +1,86 @@
+package com.devqt.wavelivewallpaper;
+
+import android.graphics.Canvas;
+import android.graphics.Movie;
+import android.os.Handler;
+import android.service.wallpaper.WallpaperService;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+import java.io.IOException;
+
+public class AnimationService extends WallpaperService {
+
+    @Override
+    public WallpaperService.Engine onCreateEngine() {
+        try {
+            Movie movie = Movie.decodeStream(
+                    getResources().getAssets().open("volna.gif"));
+
+            return new GIFWallpaperEngine(movie);
+        }catch(IOException e){
+            Log.d("GIF", "Could not load asset");
+            return null;
+        }
+    }
+
+    private class GIFWallpaperEngine extends WallpaperService.Engine {
+
+        private final int frameDuration = 20;
+
+        private SurfaceHolder holder;
+        private Movie movie;
+        private boolean visible;
+        private Handler handler;
+
+        public GIFWallpaperEngine(Movie movie) {
+            this.movie = movie;
+            handler = new Handler();
+        }
+
+        @Override
+        public void onCreate(SurfaceHolder surfaceHolder) {
+            super.onCreate(surfaceHolder);
+            this.holder = surfaceHolder;
+        }
+
+        private Runnable drawGIF = new Runnable() {
+            public void run() {
+                draw();
+            }
+        };
+
+
+        private void draw() {
+            if (visible) {
+                Canvas canvas = holder.lockCanvas();
+                canvas.save();
+
+                canvas.scale(3f, 3f);
+                movie.draw(canvas, -100, 0);
+                canvas.restore();
+                holder.unlockCanvasAndPost(canvas);
+                movie.setTime((int) (System.currentTimeMillis() % movie.duration()));
+
+                handler.removeCallbacks(drawGIF);
+                handler.postDelayed(drawGIF, frameDuration);
+            }
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            this.visible = visible;
+            if (visible) {
+                handler.post(drawGIF);
+            } else {
+                handler.removeCallbacks(drawGIF);
+            }
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            handler.removeCallbacks(drawGIF);
+        }
+    }
+}
